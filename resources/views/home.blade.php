0af42befd3c8802e87323ad0b23d7f808b6b1174
Anam Imtiaz <anam.imtiaz@zigron.com>
2016-07-24 22:43:54 +0500

    Add zigron event templates files and application

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.templates')
+@section('content')
+    <h4 class="alert_info">Welcome to Zigron Event Management System</h4>
+    @endsection
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,15 @@
+<header id="header">
+        <hgroup>
+            <h1 class="site_title"><a href="index.html">Zigron Events</a></h1>
+            <h2 class="section_title">Login Page</h2><div class="btn_view_site"></div>
+        </hgroup>
+    </header> <!-- end of header bar -->
+   {{-- <section id="secondary_bar">
+        <div class="user">
+            <p>John Doe </p>
+            <!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
+        </div>
+        <div class="breadcrumbs_container">
+            <article class="breadcrumbs"><a href="index.html">Zigron Events</a> <div class="breadcrumb_divider"></div> <a class="current">Dashboard</a></article>
+        </div>
+    </section>--}}<!-- end of secondary bar -->
